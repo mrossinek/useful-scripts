6d03dec0eb08aa8c4601d65820460d51231ca2b0
mrossinek <rmax@ethz.ch>
2020-04-08 22:00:10 +0200

    Fix output names

--- toggle_screen.sh
+++ toggle_screen.sh
@@ -9,25 +9,25 @@
 # Usage: run ./toogle_screen.sh
 # Optionally: bind this script to a keyboard shortcut
 
-numberConnected=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" | wc -l)
-numberActive=$(xrandr | grep -E " connected (primary )?[0-9]+" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" | wc -l)
+numberConnected=$(xrandr | grep " connected" | sed -e "s/\([a-zA-Z0-9]\+\) connected.*/\1/" | wc -l)
+numberActive=$(xrandr | grep -E " connected (primary )?[0-9]+" | sed -e "s/\([a-zA-Z0-9]\+\) connected.*/\1/" | wc -l)
 
 
 if (($numberConnected > $numberActive)); then
     echo "Turning inactive screens on..."
-    if xrandr | grep "HDMI2 connected"; then
-        xrandr --output HDMI2 --auto --scale 1.33x1.33 --right-of eDP1 --auto
-    elif xrandr | grep "DP1 connected"; then
-        xrandr --output DP1 --auto --right-of eDP1 --auto
+    if xrandr | grep "HDMI-2 connected"; then
+        xrandr --output "HDMI-2" --auto --scale 1.33x1.33 --right-of "eDP-1" --auto
+    elif xrandr | grep "eDP-1 connected"; then
+        xrandr --output "eDP-1" --auto --right-of "eDP-1" --auto
     else
         echo "Error."
     fi
 elif (($numberConnected == $numberActive)); then
     echo "Turning external screens off..."
-    if xrandr | grep "HDMI2 connected"; then
-        xrandr --output HDMI2 --off --output eDP1 --auto
-    elif xrandr | grep "DP1 connected"; then
-        xrandr --output DP1 --off --output eDP1 --auto
+    if xrandr | grep "HDMI-2 connected"; then
+        xrandr --output "HDMI-2" --off --output "eDP-1" --auto
+    elif xrandr | grep "eDP-1 connected"; then
+        xrandr --output "eDP-1" --off --output "eDP-1" --auto
     else
         echo "Error."
     fi
