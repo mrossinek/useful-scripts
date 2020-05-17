741c51a03353d6698cecbcfb4579a34ab3f4bded
mrossinek <rmax@ethz.ch>
2020-05-17 12:10:59 +0200

    Ensure stock data works on weekends

--- stock_watcher.py
+++ stock_watcher.py
@@ -14,21 +14,36 @@ import os
 import pandas as pd
 import yfinance as yf
 
+# read library
 with open(os.path.expanduser("~/.stocks/library.txt"), "r") as LIBRARY:
     SYMBOLS = LIBRARY.read()
 
+# get today
 TODAY = datetime.today()
-YESTERDAY = TODAY - timedelta(days=1)
+# get latest weekday
+LATEST_WEEKDAY = TODAY - timedelta(days=1)
+while LATEST_WEEKDAY.weekday() > 4:
+    LATEST_WEEKDAY -= timedelta(days=1)
+# get weekday previous to that one
+PREV_WEEKDAY = LATEST_WEEKDAY - timedelta(days=1)
+while PREV_WEEKDAY.weekday() > 4:
+    PREV_WEEKDAY -= timedelta(days=1)
+# get tomorrow
 TOMORROW = TODAY + timedelta(days=1)
-DATA = yf.download(SYMBOLS, start=f"{YESTERDAY:%F}", end=f"{TOMORROW:%F}")
-PCLOSE = DATA["Close"].loc[f"{YESTERDAY:%F}"]
-CLOSE = DATA["Close"].loc[f"{TODAY:%F}"]
 
+# download data between previous weekday and tomorrow
+DATA = yf.download(SYMBOLS, start=f"{PREV_WEEKDAY:%F}", end=f"{TOMORROW:%F}")
+# extract closing data
+PREV_CLOSE = DATA["Close"].loc[f"{PREV_WEEKDAY:%F}"]
+LATEST_CLOSE = DATA["Close"].loc[f"{LATEST_WEEKDAY:%F}"]
+
+# create results dataframe
 RESULT = pd.DataFrame()
-RESULT.insert(0, "Current", CLOSE)
-RESULT.insert(1, "Change", CLOSE - PCLOSE)
-RESULT.insert(2, "Percent", (CLOSE - PCLOSE) / CLOSE * 100.)
+RESULT.insert(0, "Current", LATEST_CLOSE)
+RESULT.insert(1, "Change", LATEST_CLOSE - PREV_CLOSE)
+RESULT.insert(2, "Percent", (LATEST_CLOSE - PREV_CLOSE) / LATEST_CLOSE * 100.)
 
+# write results
 with open(os.path.expanduser("~/.stocks/prices.txt"), "w") as PRICES:
     PRICES.write(RESULT.sort_values(by="Percent", ascending=False).to_string(header=False))
     PRICES.write(f"\n{datetime.now():%d %b %r}")
